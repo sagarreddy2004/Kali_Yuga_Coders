23044580fc05edce62bbb20a0c662da09ff3465c
divyateja050 <divyateja050@gmail.com>
2025-02-26 12:49:50 +0530

    Delete university-platform-client directory

--- university-platform-client/backend/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-module.exports = app => {
-    const auth = require('../controllers/authController');
-  
-    app.post('/api/register', auth.register);
-    app.post('/api/login', auth.login);
-  };
-  
